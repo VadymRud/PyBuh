e64d0b283b388295faaa1b3b15c2cdb700499968
Vadim Rud <rud.vadim@gmail.com>
2016-03-07 15:40:57 +0200

    added few modules

---
+++ card_product/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CardProductConfig(AppConfig):
+    name = 'card_product'
---
+++ chart_account/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChartAccountConfig(AppConfig):
+    name = 'chart_account'
