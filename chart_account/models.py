442ae8c7e6ec7699888c48edbdf3bb5b0f35d9f1
Vadim Rud <rud.vadim@gmail.com>
2016-03-08 15:45:30 +0200

    added few migrate

--- chart_account/models.py
+++ chart_account/models.py
@@ -1,3 +1,22 @@
+#chart_account
 from django.db import models
+import uuid
+from datetime import datetime
 
-# Create your models here.
+class BaseChartAccaunt(models.Model):
+    code = models.UUIDField( default=uuid.uuid4, verbose_name = 'Unique code')
+    date = models.DateTimeField(default=datetime.now, verbose_name = 'Дата Создания')
+    active = models.BooleanField(verbose_name = 'Активный', default=True)
+    name = models.CharField(max_length=255, verbose_name = 'Полное имя')
+    number = models.CharField(max_length=255, verbose_name = 'Полное имя')
+    class Meta:
+       abstract = True
+
+
+class Through(BaseChartAccaunt):
+    pass
+    #some = models.CharField(max_length=100, verbose_name = 'Полное имя')
+
+class ChartAccaunt(BaseChartAccaunt):
+    through = models.ForeignKey(Through, related_name='through___qqq')
+    
