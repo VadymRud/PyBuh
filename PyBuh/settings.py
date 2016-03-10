a541b6fc3585fa8f0bfab190a02f6b4f6d92bc8b
Vadim Rud <rud.vadim@gmail.com>
2016-03-10 11:03:56 +0200

    add few bugs

--- PyBuh/settings.py
+++ PyBuh/settings.py
@@ -75,14 +75,24 @@ WSGI_APPLICATION = 'PyBuh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'NAME': 'free_buh',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '5432',
+        'HOST': 'localhost',
+
+    },
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
--- chart_account/models.py
+++ chart_account/models.py
@@ -2,13 +2,15 @@
 from django.db import models
 import uuid
 from datetime import datetime
+from django.utils.translation import ugettext_lazy as _
+
 
 class BaseChartAccaunt(models.Model):
-    code = models.UUIDField( default=uuid.uuid4, verbose_name = 'Unique code')
-    date = models.DateTimeField(default=datetime.now, verbose_name = 'Дата Создания')
-    active = models.BooleanField(verbose_name = 'Активный', default=True)
-    name = models.CharField(max_length=255, verbose_name = 'Полное имя')
-    number = models.CharField(max_length=255, verbose_name = 'Полное имя')
+    code = models.UUIDField( default=uuid.uuid4, verbose_name = _('Unique code'))
+    date = models.DateTimeField(default=datetime.now, verbose_name = _('Date creation'))
+    active = models.BooleanField(verbose_name = _('Active'), default=True)
+    name = models.CharField(max_length=255, verbose_name = _('Full name'))
+    number = models.CharField(max_length=255, verbose_name = _('Number'))
     class Meta:
        abstract = True
 
@@ -18,5 +20,5 @@ class Through(BaseChartAccaunt):
     #some = models.CharField(max_length=100, verbose_name = 'Полное имя')
 
 class ChartAccaunt(BaseChartAccaunt):
-    through = models.ForeignKey(Through, related_name='through___qqq')
+    through = models.ForeignKey(Through, related_name='through', verbose_name = _('Through'))
     
