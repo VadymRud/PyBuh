44a0c50da0f1e4d957206362091ef8eaa5284cbd
helm2004 <rud.vadim@gmail.com>
2016-10-20 12:41:11 +0300

    added doc index

--- PyBuh/urls.py
+++ PyBuh/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
+#from documentes.views import html_to_pdf_view
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', admin.site.urls),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^rosetta/', include('rosetta.urls')),
+    url(r'^docum', include('documentes.urls'))
 
 ]
