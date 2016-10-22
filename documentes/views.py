f9431daffb193e7d0469c82acbdd720890b3bf8d
helm2004 <rud.vadim@gmail.com>
2016-10-22 12:20:46 +0300

    Addded pdf rendering

--- documentes/views.py
+++ documentes/views.py
@@ -1,20 +1,32 @@
-from django.core.files.storage import FileSystemStorage
-from django.http import HttpResponse
+﻿from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, FileResponse, Http404
 from django.template.loader import render_to_string
-
+from django.template.loader import get_template
+from django.template import Context
+import pdfkit
+import codecs
+import os
+from django.utils.encoding import smart_str
 # from weasyprint import HTML
 #
-# def html_to_pdf_view(request):
-#     paragraphs = ['first paragraph', 'second paragraph', 'third paragraph']
-#     html_string = render_to_string('documentes/index.html', {'paragraphs': paragraphs})
-#
-#     html = HTML(string=html_string)
-#     html.write_pdf(target='/tmp/mypdf.pdf');
-#
-#     fs = FileSystemStorage('/tmp')
-#     with fs.open('mypdf.pdf') as pdf:
-#         response = HttpResponse(pdf, content_type='application/pdf')
-#         response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
-#         return response
-#
-#     return response
+def html_to_pdf_view(request):
+    options = {
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+    }
+
+    template = get_template("documentes/index.html")
+    data = [1,2,3]
+    context = Context({"paragraphs": data})  # data is the context data that is sent to the html file to render the output.
+    html = template.render(context)  # Renders the template with the context data.
+    pdfkit.from_string(html, 'out.pdf', options=options)
+    try:
+        return FileResponse(open('out.pdf', 'rb'), content_type='application/pdf')
+        os.remove("out.pdf")  # remove the locally created pdf file.
+    except FileNotFoundError:
+        raise Http404()
+
+    #return response  # returns the response.
